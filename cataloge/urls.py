83327c90079e915f166228773ae6845f230f7d1f
ahmed elsenosy <cookingchess7@gmail.com>
2025-03-08 23:52:30 +0200

    Add book creation and detail views with corresponding templates

--- cataloge/urls.py
+++ cataloge/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('CreateBook',views.BookCreate.as_view(),name='CreateBook'),
+    path('detail/<int:pk>', views.BookDetail.as_view(),name='detail'),
 ]
--- cataloge/views.py
+++ cataloge/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render , HttpResponse
+from . import models
+from django.views import generic
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Hello World </h1>')
+    BooksNum = models.Book.objects.all().count()
+    BooksNumInstance = models.BookInstance.objects.all().count()
+    BooksNumAvailable = models.BookInstance.objects.filter(status__exact='a').count()
+
+    context = {
+        'BooksNum': BooksNum,
+        'BooksNumInstance': BooksNumInstance,
+        'BooksNumAvailable': BooksNumAvailable,
+    }
+    return render(request,'index.html',context)
+
+
+class BookCreate(generic.CreateView):
+    model = models.Book
+    fields = '__all__'
+    success_url = '/cataloge'
+    template_name = 'create.html'
+
+class BookDetail(generic.DetailView):
+    model = models.Book
+    template_name = 'detail.html'
+    context_object_name = 'book'
